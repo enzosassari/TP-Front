8ac6d131551497482b14d7ce59d9bfc89f3119e1
enzosassari <enzosassari@hotmail.com>
2024-07-25 21:18:37 -0300

    Varios cambios

--- productos.js
+++ productos.js
@@ -2,18 +2,18 @@
 
 let producto = [{
     nombre: "arroz",
-    cantidad: 5,
-    precio: 1500
+    cantidad: 10,
+    precio: 100
 },
 {
     nombre: "papa",
-    cantidad: 20,
-    precio: 4000
+    cantidad: 10,
+    precio: 200
 },
 {
     nombre: "limon",
-    cantidad: 40,
-    precio: 2000,
+    cantidad: 10,
+    precio: 300,
 }];
 
 
@@ -21,34 +21,47 @@ let btnArroz = document.getElementById("btnArroz");
 let btnPapa = document.getElementById("btnPapa");
 let btnLimon = document.getElementById("btnLimon");
 
-var inputValor = document.getElementById("inputArroz").value;
-var cantidadADescontar = parseInt(inputValor);
+let totalCompra = 0;
 
 btnArroz.addEventListener("click", ()=>{
-    //if (inputValor<=producto[0].cantidad){
-        producto[0].cantidad -= cantidadADescontar ;
-        document.querySelector(".arroz").innerHTML=`El producto ${producto[0].nombre} tiene un stock de ${producto[0].cantidad}`; 
-    //}else{
-     //   alert("ERROR! STOCK INSUFICIENTE");
-   // }
+    let cantidadArroz = document.getElementById("inputArroz").value;
+    if (cantidadArroz<=producto[0].cantidad){
+        producto[0].cantidad -= cantidadArroz ;
+        totalCompra += (producto[0].precio * cantidadArroz);
+        document.querySelector(".arroz").innerHTML=`El producto ${producto[0].nombre} tiene un stock de ${producto[0].cantidad}`;  
+        document.querySelector("#totalCompra").innerHTML= ("El total de la compra es : " + totalCompra); 
+    }else{
+        alert("ERROR! STOCK INSUFICIENTE");
+    }
 });
 
+
 btnPapa.addEventListener("click", ()=>{
-    producto[1].cantidad -- ;
-    document.querySelector(".papa").innerHTML=`El producto ${producto[1].nombre} tiene un stock de ${producto[1].cantidad}`; 
+    let cantidadPapa = document.getElementById("inputPapa").value;
+    if (cantidadPapa<=producto[1].cantidad){
+        producto[1].cantidad -= cantidadPapa ;
+        totalCompra += (producto[1].precio * cantidadPapa);
+        document.querySelector(".papa").innerHTML=`El producto ${producto[1].nombre} tiene un stock de ${producto[1].cantidad}`;
+        document.querySelector("#totalCompra").innerHTML= ("El total de la compra es : " + totalCompra);   
+    }else{
+        alert("ERROR! STOCK INSUFICIENTE");
+    }
  });
  
  
  btnLimon.addEventListener("click", ()=>{
-    producto[2].cantidad -- ;
-    document.querySelector(".limon").innerHTML=`El producto ${producto[2].nombre} tiene un stock de ${producto[2].cantidad}`; 
+    let cantidadLimon = document.getElementById("inputLimon").value;
+    if (cantidadLimon<=producto[2].cantidad){
+        producto[2].cantidad -= cantidadLimon ;
+        totalCompra += (producto[2].precio * cantidadLimon);
+        document.querySelector(".limon").innerHTML=`El producto ${producto[2].nombre} tiene un stock de ${producto[2].cantidad}`;  
+        document.querySelector("#totalCompra").innerHTML= ("El total de la compra es : " + totalCompra); 
+    }else{
+        alert("ERROR! STOCK INSUFICIENTE");
+    }
  });
  
  
-  
 
-
-
-document.querySelector("input").innerHTML="";
 
 
